acca9c865ec9a98b4f395fd0b70a622879c58144
Robert Dudas <roberto_dudas@yahoo.com>
2022-01-18 20:21:05 +0200

    Add project files.

---
+++ ProiectBanca/ProiectBanca.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <ContBancar.h>
+
+int main()
+{
+    ContBancar test("Dudas", "Robert", "Ro160919980212");
+    ContBancar* cont = new ContBancar("Dudas", "Robert", "Ro160919980212");
+}
