b7b5cbc91573290162aa64231a092b460bf1b055
Robert Dudas <roberto_dudas@yahoo.com>
2022-01-20 19:11:17 +0200

    Implementare meniu principal

--- ProiectBanca/ProiectBanca.cpp
+++ ProiectBanca/ProiectBanca.cpp
@@ -10,4 +10,33 @@ int main()
     std::cout << "4 -> Stergere cont\n";
     std::cout << "5 -> Eliberare/Depunere\n";
     std::cout << "6 -> Detalii cont\n";
+
+    int optiune;
+    std::cout << "Introduceti operatia pe care o doriti\n";
+    std::cin >> optiune;
+    switch(optiune)
+    {
+    case 1:
+        std::cout << "Ati ales optiunea 1\n";
+        break;
+    case 2:
+        std::cout << "Ati ales optiunea 2\n";
+        break;
+    case 3:
+        std::cout << "Ati ales optiunea 3\n";
+        break;
+    case 4:
+        std::cout << "Ati ales optiunea 4\n";
+        break;
+    case 5:
+        std::cout << "Ati ales optiunea 5\n";
+        break;
+    case 6:
+        std::cout << "Ati ales optiunea 6\n";
+        break;
+    default:
+        std::cout << "Optiune invalida\n";
+        break;
+    }
+
 }
