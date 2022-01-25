33272da89fc6a959c2109c4e6060957add140073
Robert Dudas <roberto_dudas@yahoo.com>
2022-01-25 20:44:14 +0200

    Implemented GetNumarConturi and printAllCounturi

--- ProiectBanca/ManagerConturi.h
+++ ProiectBanca/ManagerConturi.h
@@ -8,9 +8,14 @@ class ManagerConturi
 private:
 	std::vector<ContBancar*> m_listaConturi;
 	//TODO: Metoda CreateIban trebuie sa genereze un Iban unic care sa fie folosit pentru crea conturi
-	std::string CreatIban();
+	std::string CreateIban();
 public:
 	void adaugareCont();
+
+	int GetNumarConturi();
+
+	void printAllCounturi();
+
 
 };
 
